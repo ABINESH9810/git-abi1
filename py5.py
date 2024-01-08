65411540dc13cda07923c8f8cd65d27f5278a39a
abinesh <abeneesabi@gmail.com>
2024-01-08 19:54:22 +0530

    modified

--- py5.py
+++ py5.py
@@ -1,14 +1,18 @@
-import random
-import string
+generate_password(length):
+    # Define the possible characters for the password
+    letters = string.ascii_letters
+    digits = string.digits
+    symbols = string.punctuation
 
+    # Combine all characters
+    all_characters = letters + digits + symbols
 
-# Define the possible characters for the password
-letters = string.ascii_letters
-digits = string.digits
-symbols = string.punctuation
-
-# Combine all characters
-all_characters = letters + digits + symbols
-
-print(all_characters)
-print(random.choice(all_characters))
+    password = ''
+    # Generate password
+    for _ in range(length):
+        password += secrets.choice(all_characters)
+        
+    return password
+    
+new_password = generate_password(8)
+print(new_password)
